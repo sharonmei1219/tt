fe3ad65431a2c8c802e164e3fa29000c1e5aca5f
xuelan.mei <sharonmei1219@gmail.com>
2017-09-07 12:11:09 +0800

    level up

--- js/ttModal.js
+++ js/ttModal.js
@@ -1,7 +1,7 @@
 var row2Keys = ['a', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 's',';']
 var row1Keys = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o','p']
 var row3Keys = ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.','/']
-var allKeys = row2Keys + row3Keys + row1Keys
+var allKeys = row2Keys.concat(row3Keys).concat(row1Keys)
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
